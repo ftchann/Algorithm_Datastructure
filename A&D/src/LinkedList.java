248d87d9ad84b234bc889b57573636b6de879ea5
ftchann <yann_huynh@hotmail.ch>
2020-01-06 16:46:07 +0100

    LinkedList

---
+++ A&D/src/LinkedList.java
@@ -0,0 +1,22 @@
+public class LinkedList {
+    int size;
+    Node first;
+    Node last;
+    public LinkedList(){
+        size=0;
+    }
+    public void addValue(int value){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+class Node{
+    int value;
+    Node prev;
+    Node next;
+    public Node(int value){
+        this.value = value;
+    }
+}
