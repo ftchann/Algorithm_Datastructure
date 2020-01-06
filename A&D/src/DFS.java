ed0af818cb67adc20f8ca11c17444f61776dcf09
Tenzin Langdun <Tenzin Langdun@10.5.46.27>
2020-01-06 18:15:20 +0100

    dfs

---
+++ A&D/src/DFS.java
@@ -0,0 +1,43 @@
+public class DFS {
+	class Graph {
+		int[][] graph; // undirected graph represented by an adjacency matrix
+		// feel free to add any other attributes
+		boolean[] visited;
+
+		/**
+		 * The class is instantiated once for every graph.
+		 */
+		Graph(int[][] g) {
+			graph = g;
+			// initialize other attributes, if you declared any
+			visited = new boolean[g.length];
+			
+			for (int i = 0; i < g.length; ++i) {
+				visited[i] = false;
+			}
+		}
+		
+		void DfsRecursive(int node) {
+			
+			
+			visited[node] = true;
+
+			for (int i = 0; i < graph[node].length; ++i) {
+				if (graph[node][i] == 1) {
+					if (visited[i]== false) {
+						visited[i] = true;
+						DfsRecursive(i);
+					}
+				}
+			}
+			
+			
+		}
+		
+		void DfsStack(int node) {
+			
+		}
+	
+	}
+
+}
