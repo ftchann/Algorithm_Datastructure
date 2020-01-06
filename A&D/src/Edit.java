6dea92702d645b4194b9235f983b5bc0749a7c5e
yhuynh <yhuynh@student.ethz.ch>
2020-01-06 10:37:29 +0100

    reordering

---
+++ A&D/src/Edit.java
@@ -0,0 +1,28 @@
+public class Edit {
+    public static void main(String[] args) {
+        String A = "Tiger";
+        String B = "Ziege";
+        A = " " + A;
+        B = " " + B;
+        int n= A.length();
+        int m = B.length();
+        int[][] E = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            E[i][0] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            E[0][i] = i;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                int deltaij = (A.charAt(i) == B.charAt(j)) ? 1 : 0;
+                E[i][j] = min(E[i-1][j], E[i][j-1] + 1, E[i-1][j-1]+deltaij);
+            }
+        }
+        //System.out.println(Arrays.deepToString(L));
+        System.out.println(E[n-1][m-1]);
+    }
+    public static int min(int a, int b, int c){
+        return Math.min(Math.min(a,b), c);
+    }
+}
