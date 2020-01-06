8c4455d92e1729f16789b062e249e1b3eecc6dc5
yhuynh <yhuynh@student.ethz.ch>
2020-01-06 10:49:09 +0100

    edit

--- A&D/src/Edit.java
+++ A&D/src/Edit.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Edit {
     public static void main(String[] args) {
-        String A = "Tiger";
-        String B = "Ziege";
+        String B = "Tiger";
+        String A = "Ziege";
         A = " " + A;
         B = " " + B;
         int n= A.length();
@@ -16,10 +18,10 @@ public class Edit {
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 int deltaij = (A.charAt(i) == B.charAt(j)) ? 1 : 0;
-                E[i][j] = min(E[i-1][j], E[i][j-1] + 1, E[i-1][j-1]+deltaij);
+                E[i][j] = min(E[i-1][j] + 1, E[i][j-1] + 1, E[i-1][j-1]+ 1-deltaij);
             }
         }
-        //System.out.println(Arrays.deepToString(L));
+        //System.out.println(Arrays.deepToString(E));
         System.out.println(E[n-1][m-1]);
     }
     public static int min(int a, int b, int c){
