1e7ab3b862988c42d6d2b1243cde1dfe9e3e00ec
ftchann <yann_huynh@hotmail.ch>
2020-01-07 20:17:59 +0100

    median of median

--- A&D/src/QuickSelect.java
+++ A&D/src/QuickSelect.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
-
+// Really Bad Implementation of Median of Median. it still runs in O(n), but uses a lot of extra space;
 public class QuickSelect {
     public static void main(String[] args) {
         int[] A = {0,2,1,7,9,3,8,4,5,6};
-        int k = 4; // suche nach dem k. kleinsten element;
+        int k = 6; // suche nach dem k. kleinsten element;
         int ret = quickselect(A,0,A.length-1,k);
         System.out.println(ret);
     }
@@ -21,6 +21,8 @@ public class QuickSelect {
     private static int split(int[] A, int l, int r) {
         int i=l;
         int j=r-1;
+        int pivot = pivot(A,l,r);
+        Swap(A,pivot,r);
         int p=A[r];//Pivot auswählen
         do{
             while(i<r && A[i]<p){
@@ -32,6 +34,49 @@ public class QuickSelect {
         }while((i<j));
         Swap(A,i,r);
         return i;
+    }
+    // Median of Medians fuck inplace, i won't rewrite whole code
+    public static int pivot(int[] A, int l, int r){
+        int[] B = new int[r-l+1];
+        for (int i = 0; i < B.length; i++) {
+            B[i] = A[l+i];
+        }
+        return median(B);
+    }
+    public static int median(int[] B){
+        int plus = 0;
+        if(B.length % 5 != 0){
+            plus++;
+        }
+        int cpointer = 0;
+        int C[] = new int[B.length/5+plus];
+        for (int i = 0; i < B.length; i+=5) {
+            int toadd;
+            if(B.length-i >=5){
+                int[] D = new int[5];
+                for (int j = 0; j < 5; j++) {
+                    D[j]=B[i+j];
+                }
+                toadd = partition5(D);
+            }else{
+                int[] D = new int[B.length-i];
+                for (int j = 0; j < B.length-i; j++) {
+                    D[j] = B[j+i];
+                }
+                toadd = partition5(D);
+            }
+            C[cpointer] = toadd;
+            cpointer++;
+        }
+        if(C.length == 1){
+            return C[0];
+        }
+        return median(C);
+    }
+    public static int partition5(int[] A){
+        Arrays.sort(A);
+        int n=A.length;
+        return A[n/2];
     }
     private static void Swap(int[] A, int i, int largest) {
         int temp=A[i];
