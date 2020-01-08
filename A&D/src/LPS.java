26a43f10c67bff8bbbe72513ffc7c7b13adec8eb
ftchann <yann_huynh@hotmail.ch>
2020-01-08 18:17:01 +0100

    manachers algortihm

--- A&D/src/LPS.java
+++ A&D/src/LPS.java
@@ -1,10 +1,68 @@
+import java.util.Arrays;
+
 public class LPS {
-    // Longest palindromic substring, Manchesters algorithm will come later.
+    // Longest palindromic substring, Manachers algorithm will come later.
     // O(n^2) solution
     public static void main(String[] args) {
         String S = "GOKAYAKING";
-        System.out.println(substringDP(S));
+        System.out.println(manachersAlgorithm(S));
     }
+    public static String manachersAlgorithm(String s){
+        String toreturn = "";
+        int N = s.length();
+        String str = getModifiedString(s, N);
+        int len = (2*N) + 1;
+        assert len == str.length();
+        int[] P = new int[len];
+        int c = 0;
+        int r = 0;
+        int maxLen = 0;
+        for (int i = 0; i < len; i++) {
+            int mirror = (2 * c) -i;
+            if(i<r){
+                P[i] = Math.min(r-i,P[mirror]);
+            }
+            int a = i + (1 + P[i]);
+            int b = i - (1 + P[i]);
+            while(a<len &&  b>=0 && str.charAt(a) == str.charAt(b)){
+                P[i]++;
+                a++;
+                b--;
+            }
+            if(i + P[i] > r){
+                c = i;
+                r = i + P[i];
+                if(P[i] > maxLen){
+                    maxLen = P[i];
+                }
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if(P[i] == maxLen){
+                toreturn = remodifie(str.substring(i-maxLen, i + maxLen));
+            }
+        }
+        return toreturn;
+    }
+    public static String remodifie(String s){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != '#'){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static String getModifiedString(String s, int N){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append("#");
+            sb.append(s.charAt(i));
+        }
+        sb.append("#");
+        return sb.toString();
+    }
+
     public static String substringDP(String S){
         int n = S.length();
         boolean[][] dp= new boolean[n][n];
